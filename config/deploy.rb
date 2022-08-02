7ae217650adc960974cb09e7463fa02b84238759
Jackson Pires <jack@jack.com>
2022-08-02 13:41:52 +0000

    Alterando ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,4 @@ append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "
 set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
