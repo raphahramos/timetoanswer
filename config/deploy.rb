297efc887c96fa822b8d957a573b832b4f897f01
Jackson Pires <jack@jack.com>
2022-08-03 13:05:23 +0000

    Config Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "storage", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
